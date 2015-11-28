f89bf6d423ae08de15c0631d31d95bdff8820d70
Shifar Shifz <shifar.shifz@gmail.com>
2015-11-28 20:54:32 +0530

    Everything is ok

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
+
 $(document).ready(function(){
-
+	
 	console.log("Quora extension started");
-
-	var isContentAppended = false;
+	var isContentAppended = true;
+	hideTopQuoraContents();
+	
+	$("div#feed_visibility_wrapper").slideDown(500);
 
 	$("#feed_visibility_wrapper").bind("DOMSubtreeModified", function() {
 		isContentAppended = true;
@@ -11,18 +14,24 @@ $(document).ready(function(){
 	setInterval(hideTopQuoraContents,1000);
 
 	function hideTopQuoraContents(){
+		
 		if(!isContentAppended){
-
+			console.log("Content not appended");
+			return;
 		}		
+		
 		$('div.EventHeader').each(function(i, heading) {
-        var headingData = heading.innerHTML;
-        var hasTopContent = headingData.indexOf("Top Content on Quora")!=-1;
-        
-        if(hasTopContent){
-            $(heading).closest(".pagedlist_item").css('opacity','0.2');
-        }
-        console.log("Managed "+i);
-	});
+			var headingData = heading.innerHTML;
+			var hasTopContent = headingData.indexOf("Top Content on Quora")!=-1;
+			
+			if(hasTopContent){
+				$(heading).closest(".pagedlist_item").hide();
+			}
+			console.log("Managed "+i);
+		});
+		
+		console.log("Finished");
+		isContentAppended = false;
 	}
 
 });
