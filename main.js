eae20836dfb707e1ce0b8c545e18096ca3e73fa5
Shifar Shifz <shifar.shifz@gmail.com>
2015-11-28 19:29:10 +0530

    The algorith needs to be modified

--- main.js
+++ main.js
@@ -1,50 +1,22 @@
-var QuoraExtension =
-{
-    /* Initialize Quora Extension */
-    main: function()
-    {
-        QuoraExtension.hideContent();
-    },
+$(document).ready(function(){
 
-    /* Hide Selected Content From Quora */
-    hideContent: function()
-    {
-        $(document).ready(
-            function()
-            {
-                //Run the clean out as soon as possible
-                QuoraExtension.hideTopContent();
+	console.log("Quora extension started");
+	
+	$("#feed_visibility_wrapper").bind("DOMSubtreeModified", function() {
+	    hideTopQuoraContents();
+	});
 
-                /*
-                    Now, we add a listener for when the DOM tree is updated,
-                    when it is, we check for more content to hide.
+	function hideTopQuoraContents(){
+		$('div.EventHeader').each(function(i, heading) {
+        var headingData = heading.innerHTML;
+        console.log("Heading is "+headingData);
+        var hasTopContent = headingData.indexOf("Top Content on Quora")!=-1;
+        
+        if(hasTopContent){
+            $(heading).closest(".pagedlist_item").css('opacity','0.2');
+        }
+        console.log("Has top content = "+hasTopContent);
+	});
+	}
 
-                    Timeouts are used to prevent infinite loops of removals
-                    Removal of content can sometimes trigger another check,
-                    so timeouts prevent us from using too much processing
-                 */
-                var hideTopContentTimeout = null;
-
-                document.addEventListener("DOMSubtreeModified", function() {
-                    if(hideTopContentTimeout) {
-                        clearTimeout(hideTopContentTimeout);
-                    }
-                    hideTopContentTimeout = setTimeout(QuoraExtension.hideTopContent, 500);
-                }, false);
-            }
-        );
-    },
-
-    /* Hide Top Content sections */
-    hideTopContent: function()
-    {
-        var $topContent = jQuery("div:contains('Top Content on Quora')").closest('.pagedlist_item');
-        $topContent.hide();
-    }
-};
-
-// Initialize QuoraExtension if JQuery is available
-if(!!jQuery)
-{
-    QuoraExtension.main();
-}
+});
