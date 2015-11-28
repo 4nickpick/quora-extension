e961655a29b4647f022f43bb4f186d6ac3ed7dc4
Shifar Shifz <shifar.shifz@gmail.com>
2015-11-28 19:39:14 +0530

    BACKUP

--- main.js
+++ main.js
@@ -1,21 +1,27 @@
 $(document).ready(function(){
 
 	console.log("Quora extension started");
-	
+
+	var isContentAppended = false;
+
 	$("#feed_visibility_wrapper").bind("DOMSubtreeModified", function() {
-	    hideTopQuoraContents();
+		isContentAppended = true;
 	});
 
+	setInterval(hideTopQuoraContents,1000);
+
 	function hideTopQuoraContents(){
+		if(!isContentAppended){
+
+		}		
 		$('div.EventHeader').each(function(i, heading) {
         var headingData = heading.innerHTML;
-        console.log("Heading is "+headingData);
         var hasTopContent = headingData.indexOf("Top Content on Quora")!=-1;
         
         if(hasTopContent){
             $(heading).closest(".pagedlist_item").css('opacity','0.2');
         }
-        console.log("Has top content = "+hasTopContent);
+        console.log("Managed "+i);
 	});
 	}
 
