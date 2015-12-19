6b13c4ca39bab487f88ceb91384e7d1c776c5961
Shifar <shifar.shifz@gmail.com>
2015-12-19 11:18:49 +0530

    Changed core catching element.
    Quora changed 'Top Content on Quora' to 'Popular on Quora'

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 
 //Hide @first load
-$("div.EventHeader:contains('Top Content on Quora')").closest('.pagedlist_item').hide();
+$("div.EventHeader:contains('Popular on Quora')").closest('.pagedlist_item').hide();
 
 $(document).ready(function(){
 	
 	//Hide @page finished loading
-	$("div.EventHeader:contains('Top Content on Quora')").closest('.pagedlist_item').hide();
+	$("div.EventHeader:contains('Popular on Quora')").closest('.pagedlist_item').hide();
 	
 	//Hide @oneach load
 	$("#feed_visibility_wrapper").bind("DOMSubtreeModified", function() {
-		$("div.EventHeader:contains('Top Content on Quora')").closest('.pagedlist_item').hide();
+		$("div.EventHeader:contains('Popular on Quora')").closest('.pagedlist_item').hide();
 	});
 
 });
